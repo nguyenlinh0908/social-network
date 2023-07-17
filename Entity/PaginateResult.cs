b173669080ed4e295b0a082c333413d0b66cb4c2
linhnv-comartek <linhnv@comartek.com>
2023-07-18 00:35:59 +0700

    create post and display there into new feed

--- Entity/PaginateResult.cs
+++ Entity/PaginateResult.cs
@@ -2,8 +2,10 @@ namespace social_network.Entity;
 public class PaginateResult<T>
 {
     public List<T> data { get; set; }
-    public int totalRecords { get; set; }
+    public int? totalRecords { get; set; }
     public int limit { get; set; }
 
-    public int currentPage { get; set; }
+    public int? currentPage { get; set; }
+
+    public int? currentId { get; set; }
 }
